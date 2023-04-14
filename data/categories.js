7ffe958d162bc4f790da85280ff17a97f27db043
khawaji <34907189+khawaji@users.noreply.github.com>
2023-04-14 17:34:01 +0300

    update index

--- data/categories.js
+++ data/categories.js
@@ -1,1 +1,1 @@
-const categories = {"Eid Celebration": {"120.png": {"name": {"x": 515, "y": 851}, "color": "#fdeca6"}, "130.png": {"name": {"x": 382, "y": 818}, "color": "#56047c"}}, "Graduation": {"12.png": {"name": {"x": 515, "y": 851}, "color": "#fdeca6"}, "13.png": {"name": {"x": 382, "y": 818}, "color": "#56047c"}}}
+const categories = {"Eid Cards": {"120.png": {"name": {"x": 515, "y": 851}, "color": "#fdeca6"}, "130.png": {"name": {"x": 382, "y": 818}, "color": "#56047c"}}}
