14229937031ea08f85791f8f5a4bbd33f9f5120c
khawaji <34907189+khawaji@users.noreply.github.com>
2023-04-14 16:42:58 +0300

    update
    space issue

--- script/load_selection.js
+++ script/load_selection.js
@@ -1,4 +1,4 @@
-const queryString = window.location.search.split("=")[1];
+const queryString = window.location.search.split("=")[1].replace(/%20/, " ");
 var mainElement = document.querySelector("main");
 Object.keys(categories[queryString]).forEach(cat => {
     mainElement.innerHTML += selection_item_prefab.replace(/NAME/g, cat).replace(/KAT/g, queryString);
