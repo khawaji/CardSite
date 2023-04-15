7ccfed71a372a867f66f1f38921dbeff2cee158b
khawaji <34907189+khawaji@users.noreply.github.com>
2023-04-15 20:47:45 +0300

    add new card

--- data/categories.js
+++ data/categories.js
@@ -1,1 +1,1 @@
-const categories = {"Eid Cards": {"120.png": {"name": {"x": 515, "y": 851}, "color": "#fdeca6"}, "130.png": {"name": {"x": 382, "y": 818}, "color": "#56047c"}}}
+const categories = {"Eid Cards": {"120.png": {"name": {"x": 515, "y": 851}, "color": "#fdeca6"}, "130.png": {"name": {"x": 382, "y": 818}, "color": "#56047c"}, "design.png": {"name": {"x": 0, "y": 0}, "color": "#000000"}}}
